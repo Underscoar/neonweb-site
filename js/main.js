cd330cb0dcfe0f24d0ea54b7641aaef683101f6e
Arne Reijntjes <arne123@live.nl>
2023-02-16 21:32:07 +0100

    Verder met homepage

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 'use strict';
 
+window.addEventListener("scroll", checkScrollPos, { passive: true });
+
+function checkScrollPos() {
+    if (window.scrollY > 0) {
+        document.querySelector('nav').classList.add('scroll-nav');
+    }
+    else {
+        document.querySelector('nav').classList.remove('scroll-nav');
+    }
+}
+
+checkScrollPos();
+
 let allTabBtns = document.querySelectorAll('.tab-view button');
 allTabBtns.forEach((element) => {
-    element.addEventListener('click', function() {
+    element.addEventListener('click', function () {
         document.querySelectorAll('.tab-view button').forEach((item) => { item.classList.remove('active'); })
         document.querySelectorAll('.tab-content .tab').forEach((item) => { item.classList.remove('active'); })
         document.querySelector(`#tab-${element.dataset.tabtoggle}`).classList.add('active');
